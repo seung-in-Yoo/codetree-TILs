fbb316563812a28af0cbf2c52137e499448d0872
유승인 <144124353+seung-in-Yoo@users.noreply.github.com>
2024-09-14 02:58:39 +0900

    Title: 입력받아 연산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240914/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    cin >> a;
+    cin >> b; 
+
+    cout << a+87 << endl;
+    cout << b%10 << endl;
+    return 0;
+}
