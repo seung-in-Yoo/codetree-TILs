4a9dddc4ac7aaf757caff715256ede0d234a1ed8
유승인 <144124353+seung-in-Yoo@users.noreply.github.com>
2024-09-15 02:42:30 +0900

    Title: 살 수 있는 물건 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240915/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n>=3000) {
+        cout << "book" << endl;
+    }
+    else if (n>=1000) {
+        cout << "mask" << endl;
+    }
+    else if (n>=500) {
+        cout << "pen" << endl;
+    }
+    else {
+        cout << "no" << endl;
+    }
+    return 0;
+}
