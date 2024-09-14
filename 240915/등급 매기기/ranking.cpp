ea614b3bf52fed4abff1aabad98c6c1f58949cd5
유승인 <144124353+seung-in-Yoo@users.noreply.github.com>
2024-09-15 02:37:49 +0900

    Title: 등급 매기기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240915/등급 매기기/ranking.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+    if (num>=90) {
+        cout << "A" << endl;
+    }
+    else if (num>=80) {
+        cout << "B" << endl;
+    }
+    else if (num>=70) {
+        cout << "C" << endl;
+    }
+    else if (num>=60) {
+        cout << "D" << endl;
+    }
+    else {
+        cout << "F" << endl;
+    }
+    return 0;
+}
