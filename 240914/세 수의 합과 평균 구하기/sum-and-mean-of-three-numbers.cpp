2b315721f0b019e5aadf889edc56645182c74a03
유승인 <144124353+seung-in-Yoo@users.noreply.github.com>
2024-09-14 20:19:47 +0900

    Title: 세 수의 합과 평균 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240914/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int c;
+    int avg;
+
+    cin >> a >> b >> c;
+    cout << a+b+c << endl;
+    cout << (a+b+c)/3 << endl;
+    return 0;
+}
