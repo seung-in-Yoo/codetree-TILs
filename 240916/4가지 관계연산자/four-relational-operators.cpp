7f8c97fa55a7bd2e98f3c600b3dfe5fe101ebffd
유승인 <144124353+seung-in-Yoo@users.noreply.github.com>
2024-09-16 19:35:39 +0900

    Title: 4가지 관계연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240916/4가지 관계연산자/four-relational-operators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << (a>=b) << endl;
+    cout << (a>b) << endl;
+    cout << (b>=a) << endl;
+    cout << (b>a) << endl;
+    return 0;
+}
