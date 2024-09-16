84897271aa3c64fc1c9bbd154d390d5e30fb7bed
유승인 <144124353+seung-in-Yoo@users.noreply.github.com>
2024-09-16 19:58:02 +0900

    Title: 세 정수의 최솟값, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240916/세 정수의 최솟값/minimum-of-three-numbers.py
+++ 240916/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -4,11 +4,11 @@ a = int(arr[0])
 b = int(arr[1])
 c = int(arr[2])
 
-if a<b and a<c:
+if a<=b and a<=c:
     print(a)
-elif b<a and b<c:
+elif b<=a and b<=c:
     print(b)
-elif c<a and c<a:
+elif c<=a and c<=a:
     print(c)
-else:
+else: 
     print("답이 없어요~!!")
