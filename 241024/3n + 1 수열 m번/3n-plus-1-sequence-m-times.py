c94c0f9fbfc40a4f5ff4921ce03be85f33292d6b
유승인 <144124353+seung-in-Yoo@users.noreply.github.com>
2024-10-24 03:46:40 +0900

    Title: 3n + 1 수열 m번, Time: 113ms, Memory: 25MB, Status: Passed - CodeTree

--- 241024/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
+++ 241024/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
@@ -1,15 +1,14 @@
-m = int(input())
-n = int(input())
-count = 0 
+m = int(input())  
 
-while True:
-    if n % 2 == 0:
-        count += 1
-        n= n / 2
-    elif n % 2 == 1:
-        count += 1
-        n = n*3 + 1
-    if n==1:
-        break
+for _ in range(m): 
+    n = int(input())  
+    count = 0  
 
-print(count)
+    while n != 1:  
+        if n % 2 == 0:
+            n = n // 2  
+        else:
+            n = n * 3 + 1  
+        count += 1  
+    
+    print(count)
