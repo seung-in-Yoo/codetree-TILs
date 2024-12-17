14b081888ef90033b9ddc595f0efc4db75401cdd
유승인 <144124353+seung-in-Yoo@users.noreply.github.com>
2024-12-17 21:36:23 +0900

    Title: 격자 반대로 채우기, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241217/격자 반대로 채우기/grid-anti-fill.py
@@ -0,0 +1,19 @@
+n = int(input())
+mat = [[0] * n for _ in range(n)]
+
+first = 1  
+for col in range(n - 1, -1, -1):  
+    if col % 2 == 1:  
+        for row in range(n -1, -1, -1):
+            mat[row][col] = first
+            first += 1
+    else:  
+        for row in range(n):
+            mat[row][col] = first
+            first += 1
+
+for row in mat:
+    print(" ".join(map(str, row)))
+
+
+
