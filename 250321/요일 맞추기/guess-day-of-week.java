01b3490f3db7689c81fefbc45fadcacb91ad1a50
유승인 <144124353+seung-in-Yoo@users.noreply.github.com>
2025-03-21 14:28:33 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 250321/요일 맞추기/guess-day-of-week.java
+++ 250321/요일 맞추기/guess-day-of-week.java
@@ -1,36 +1,31 @@
-import java.util.Scanner;
+iimport java.util.Scanner;
 
 public class Main {
+    public static int numOfDays(int m, int d) {
+        int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int totalDays = 0;
+        
+        for(int i = 1; i < m; i++)
+            totalDays += days[i];
+        totalDays += d;
+        
+        return totalDays;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int m1 = sc.nextInt();
         int d1 = sc.nextInt();
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
-
         
-        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-       
-        String[] weekDays = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-
         
-        int latedate = 0;
-
-        if (m1 == m2) {
-            latedate = d2 - d1;
-        } else {
-            latedate += daysInMonth[m1 - 1] - d1;
-            
-            for (int i = m1; i < m2 - 1; i++) {
-                latedate += daysInMonth[i];
-            }
-            
-            latedate += d2;
-        }
-
-        int index = (latedate % 7 + 7) % 7;
-        System.out.println(weekDays[(7 + index) % 7]);  
+        int diff = numOfDays(m2, d2) - numOfDays(m1, d1);
+        
+        while(diff < 0)
+            diff += 7;
+        
+        String[] dayOfWeek = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        System.out.print(dayOfWeek[diff % 7]);
     }
 }
